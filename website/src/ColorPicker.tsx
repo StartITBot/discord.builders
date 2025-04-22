06b49cc161e37ffd5089bf5fb5cd76482f6113cb
kpodp0ra <podpora@kacper.link>
2025-04-22 19:34:52 +0200

    little change style

--- website/src/ColorPicker.tsx
+++ website/src/ColorPicker.tsx
@@ -10,6 +10,12 @@ export const ColorPicker: ColorPickerType = ({hexColor, onChange}) => {
             color={hexColor}
             disableAlpha={true}
             onChange={(ev) => onChange(parseInt(ev.hex.replace("#", ""), 16))}
+            presetColors={[
+                '#1abc9c', '#11806a', '#57F287', '#2ecc71', '#1f8b4c', '#3498db',
+                '#206694', '#9b59b6', '#71368a', '#e91e63', '#ad1457', '#f1c40f',
+                '#c27c0e', '#e67e22', '#a84300', '#ED4245', '#e74c3c', '#992d22',
+                '#7289da', '#5865F2', '#99aab5', '#36393F', '#EB459E', '#FEE75C'
+            ]}
         />
     </div>
 }
