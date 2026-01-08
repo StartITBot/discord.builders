d730cc6d7642330cffde6b410b58878886557a13
Kacper Podpora <podpora@kacper.link>
2026-01-08 06:34:47 +0100

    feat: video support

--- components-sdk/src/polyfills/files.ts
+++ components-sdk/src/polyfills/files.ts
@@ -1,4 +1,4 @@
 
-export type getFileType = (fileName: string) => Blob | MediaSource
+export type getFileType = (fileName: string) => Blob
 export type setFileType = (randomName: string, file: File) => Promise<string | null>
 export type getFileNameType = (url: string) => string | null;
--- components-sdk/src/utils/useFileUpload.ts
+++ components-sdk/src/utils/useFileUpload.ts
@@ -5,18 +5,23 @@ import {stateKeyType, StateManager} from "../polyfills/StateManager";
 import {uuidv4} from "./randomGen";
 import {getFileType, setFileType} from "../polyfills/files";
 
+let N = ["image/jpeg", "image/png", "image/webp", "image/gif", "image/avif", "video/quicktime", "video/mp4", "video/webm"];
+// Extensions: ['.png', '.jpg', '.jpeg', '.jfif', '.webp', '.gif', '.avif', '.mp4', '.webm', '.mov']
+
 export function useFileUpload(
     state: string,
     stateKey: stateKeyType,
     getFile: getFileType,
     setFile: setFileType,
-    stateManager: StateManager
+    stateManager: StateManager,
+    videoSupport: boolean
 ) {
     const [src, setSrc] = useState(() => state || ThumbnailIcon);
+    const [isVideo, setIsVideo] = useState<boolean | null>(null);
 
     const { openFilePicker: openFileSelector } = useFilePicker({
         multiple: false,
-        accept: ['.png', '.jpg', '.jpeg'],
+        accept: videoSupport ? N : N.filter(t => !t.startsWith("video/")).join(','),
         readFilesContent: false,
         onFilesSelected: async ({ plainFiles }) => {
             if (!plainFiles) return;
@@ -30,24 +35,35 @@ export function useFileUpload(
 
     useEffect(() => {
         if (!state.startsWith("attachment://")) {
+            setIsVideo(state.includes(".mp4") || state.includes(".webm") || state.includes(".mov"));
             setSrc(state);
             return;
         }
 
         const fileName = state.slice(13);
         try {
-            const objectURL = URL.createObjectURL(getFile(fileName));
+            const file = getFile(fileName);
+            if (file == null || !N.includes(file.type)) {
+                console.error("File not found or unsupported type:", fileName, file?.type);
+                setSrc(ThumbnailIcon);
+                setIsVideo(null);
+                return;
+            }
+
+            const objectURL = URL.createObjectURL(file);
             setSrc(objectURL);
+            setIsVideo(file.type.startsWith("video/"));
             return () => URL.revokeObjectURL(objectURL)
         } catch (e) {
             console.error(e);
             setSrc(ThumbnailIcon);
+            setIsVideo(null);
         }
 
     }, [state]);
 
     return {
-        src, setSrc, openFileSelector
+        src, setSrc, openFileSelector, isVideo
     }
 
 }
